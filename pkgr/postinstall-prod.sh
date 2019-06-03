7286d4e1043f751dc664fbd0f8da7c79c34e6246
wileyday <wiley@nurigo.net>
2019-06-03 17:50:16 +0900

    lock 파일 삭제 명령 추가

--- pkgr/postinstall-prod.sh
+++ pkgr/postinstall-prod.sh
@@ -3,7 +3,10 @@ sudo service chrony restart
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 sudo apt-get update
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
 sudo rm /var/lib/dpkg/lock
+sudo dpkg --configure -a
 sudo apt-get install -y mongodb-org=3.6.3 mongodb-org-server=3.6.3 mongodb-org-shell=3.6.3 mongodb-org-mongos=3.6.3 mongodb-org-tools=3.6.3
 sudo service mongod start
 sudo solapi-desktop-app-demo scale web=1
